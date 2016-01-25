9a5f6c8c2da10de6bb28133185b84db883a0445f
Alejandro Corredor <aecorredor93@yahoo.com>
2016-01-25 13:08:10 -0500

    Finished Caesar Cipher

---
+++ caesar_cipher.rb
@@ -0,0 +1,21 @@
+  def caesar_cipher(string, shift_factor)
+    (0..(string.length - 1)).each do |index|
+        wrap_value = 0
+        if ('A'..'Z').to_a.include?(string[index])
+          wrap_value = 65
+        elsif ('a'..'z').to_a.include?(string[index])
+          wrap_value = 97
+        end
+        new_value = (((string[index].ord - wrap_value) + shift_factor) % 26) + wrap_value
+        string[index] = new_value.chr if wrap_value == 65 || wrap_value == 97
+    end
+    puts string
+    return string
+  end
+
+  puts "enter string: "
+  input = gets.chomp
+  puts "enter shift factor: "
+  shift = gets.chomp.to_i
+
+  caesar_cipher(input, shift)
